f8bab5d0a809ba557d9d53f8b0bc9078796a1fed
Abolfazl Diyanat <ab.diynat@gmail.com>
2024-09-11 00:46:24 +0330

    Add IUST tempelate

---
+++ examples/نمونه پایان‌نامه دانشگاه علم و صنعت/Code/code3.m
@@ -0,0 +1,9 @@
+clc
+clear all
+close all
+
+% define a continuous function
+f = '4*sin(2*pi*t)';
+
+% plot a figure
+ezplot(f);
---
+++ examples/نمونه پایان‌نامه دانشگاه علم و صنعت/Code/prog3.m
@@ -0,0 +1,9 @@
+% Initialization
+clc
+clear all
+close all
+
+n = 100000;
+p = .5;
+ means = binornd(n,p,1000,1);
+
